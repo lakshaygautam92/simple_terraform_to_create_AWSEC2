e6a7ca4f46cda4f85d9e4f1221b60ec6589e7a01
lakshay <lakshay.gautam@outlook.com>
2023-10-12 02:54:51 +0530

    edit files

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "my_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- # outbound from jenkis server
+ # outbound from jenkins server
   egress {
     from_port   = 0
     to_port     = 65535
