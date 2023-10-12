7478d5ecba83bb40813b442ed2f3537638710435
lakshay <lakshay.gautam@outlook.com>
2023-10-12 15:29:53 +0530

    changes keypair name

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "C:/Users/lakshay.gautam/Desktop/Important/EktaKey.pem"
+  default     =  "EktaKey"
 }
 
 variable "instance_type" {
